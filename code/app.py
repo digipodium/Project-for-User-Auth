cff41f37424f1b4fe03e63e4a60bc666ca46e21b
zaid kamil <xaidmetamorphos@gmail.com>
2020-11-01 13:54:35 +0530

    added html pages and routes

---
+++ code/app.py
@@ -0,0 +1,38 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import engine
+from sqlalchemy.orm import sessionmaker
+from project_orm import User
+
+from flask import Flask,session,flash,redirect,render_template,url_for
+
+app = Flask(__name__)
+engine = create_engine('sqlite:///database.db')
+Session = sessionmaker(bind=engine)
+sess = Session()
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return render_template('index.html',title='login')
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    return render_template('signup.html',title='register')
+
+@app.route('/forgot',methods=['GET','POST'])
+def forgot():
+    return render_template('forgot.html',title='forgot password')
+
+@app.route('/home',methods=['GET','POST'])
+def home():
+    return render_template('home.html',title='Home')
+
+@app.route('/about')
+def home():
+    return render_template('about.html',title='About Us')
+
+@app.route('/logout')
+def logout():
+    return redirect('/')
+
+
