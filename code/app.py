58b5d8c7d8d7d247cbef922e4fedee889012ace1
zaid kamil <xaidmetamorphos@gmail.com>
2020-11-01 13:59:43 +0530

    updated app.py and base.html

--- code/app.py
+++ code/app.py
@@ -28,11 +28,14 @@ def home():
     return render_template('home.html',title='Home')
 
 @app.route('/about')
-def home():
+def about():
     return render_template('about.html',title='About Us')
 
 @app.route('/logout')
 def logout():
     return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
